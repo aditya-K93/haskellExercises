2dc02884dbaa883dba96d8594fab2e05c37662dc
aditya-K93 <aditya.kr.singh21@gmail.com>
2018-10-30 00:55:04 +0530

    renamed to provide familiar api

--- mergesort.hs
+++ mergesort.hs
@@ -1,7 +1,7 @@
-mergeSort :: ([a] -> [a] -> [a]) -> [a] -> [a]
-mergeSort merge xs
+mergeSortHelper :: ([a] -> [a] -> [a]) -> [a] -> [a]
+mergeSortHelper merge xs
  | length xs < 2 = xs
- | otherwise = merge (mergeSort merge firstHalf) (mergeSort merge otherHalf)
+ | otherwise = merge (mergeSortHelper merge firstHalf) (mergeSortHelper merge otherHalf)
   where firstHalf = take ( length xs `div` 2) xs
         otherHalf = drop (length xs `div` 2) xs
 
@@ -12,3 +12,6 @@ merge xs [] = xs
 merge (x1:xs1) (x2:xs2)
  | x1 <= x2 = x1 : merge (xs1) (x2:xs2)
  | otherwise = x2: merge (x1:xs1) (xs2)
+
+mergeSort :: Ord a => [a] -> [a]
+mergeSort xs = mergeSortHelper merge xs
