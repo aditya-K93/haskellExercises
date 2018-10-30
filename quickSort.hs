358fafc88f7f24c3c1ab03ed650b9e4ecd03a8ed
aditya-K93 <aditya.kr.singh21@gmail.com>
2018-10-31 03:34:06 +0530

    added comments

--- quickSort.hs
+++ quickSort.hs
@@ -1,7 +1,9 @@
 -- a hacky yet neat although partial implementation of quicksort in haskell
 -- please not this is not the originally proposed quicksort algorithm as it does not sort in-place.
+-- ++ concat is slow, choice of pivot being 1st element always is a poor choice. Should be random
 -- an inplace implementation is rather tedious in haskell due to default immutability with vanilla dtypes
 -- do not use in production as this is order of magnitudes slower than correponding C implementation
+
 
 quickSort :: Ord a => [a] -> [a]
 quickSort [] = []
